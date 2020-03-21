2c72d4d2c0172c0e47804717ddd3d73b77449781
Labnan <khalidmasum@iut-dhaka.edu>
2020-03-21 22:19:52 +0600

    refactor: created new method from the codes of addMouseRelease options

--- src/NewBaghbondi/BoardListener.java
+++ src/NewBaghbondi/BoardListener.java
@@ -22,45 +22,49 @@ public class BoardListener{
 
     public void addMouseReleaseOptions(Piece piece) {
         piece.setOnMouseReleased(e -> {
-                    System.out.println("-----Called makePiece() on mouse-----");
-                    int newHorizontal = pixelToBoard(piece.getLayoutX());
-                    int oldHorizontal = pixelToBoard(piece.getOldHorizontal());
-                    int newVertical = pixelToBoard(piece.getLayoutY());
-                    int oldVertical = pixelToBoard(piece.getOldVertical());
-                    MoveResult result = tryMove(piece, newHorizontal, newVertical);
-                    if(turn == 0){
-                        if (piece.getPieceType()==PieceTypeEnum.TIGER){piece.abortMove(); return;}
-                    }
-                    else if(turn==1) {
-                        if(!(piece.getPieceType()==PieceTypeEnum.TIGER)){piece.abortMove(); return;}
-                    }
-
-
-                    switch (result.getType()) {
-                        case NONE:
-                            piece.abortMove();
-                            break;
-                        case NORMAL:
-                            piece.move(newHorizontal, newVertical);
-                            System.out.println("Normal move from:: " + oldHorizontal + ", " + oldVertical);
-                            board[oldHorizontal][oldVertical].setPiece(null);
-                            board[newHorizontal][newVertical].setPiece(piece);
-                            turn = (turn+1)%2;
-                            break;
-                        case KILL:
-                            piece.move(newHorizontal, newVertical);
-                            board[oldHorizontal][oldVertical].setPiece(null);
-                            board[newHorizontal][newVertical].setPiece(piece);
-                            Piece killedPiece = result.getPiece();
-                            board[pixelToBoard(killedPiece.getOldHorizontal())][pixelToBoard(killedPiece.getOldVertical())].setPiece(null);
-                            pieceGroup.getChildren().remove(killedPiece);
-                            turn = (turn+1)%2;
-                            break;
-
-                    }
+                    makeMove(piece);
                 }
         );
 }
+
+    private void makeMove(Piece piece) {
+        System.out.println("-----Called makePiece() on mouse-----");
+        int newHorizontal = pixelToBoard(piece.getLayoutX());
+        int oldHorizontal = pixelToBoard(piece.getOldHorizontal());
+        int newVertical = pixelToBoard(piece.getLayoutY());
+        int oldVertical = pixelToBoard(piece.getOldVertical());
+        MoveResult result = tryMove(piece, newHorizontal, newVertical);
+        if(turn == 0){
+            if (piece.getPieceType()== PieceTypeEnum.TIGER){piece.abortMove(); return;}
+        }
+        else if(turn==1) {
+            if(!(piece.getPieceType()==PieceTypeEnum.TIGER)){piece.abortMove(); return;}
+        }
+
+
+        switch (result.getType()) {
+            case NONE:
+                piece.abortMove();
+                break;
+            case NORMAL:
+                piece.move(newHorizontal, newVertical);
+                System.out.println("Normal move from:: " + oldHorizontal + ", " + oldVertical);
+                board[oldHorizontal][oldVertical].setPiece(null);
+                board[newHorizontal][newVertical].setPiece(piece);
+                turn = (turn+1)%2;
+                break;
+            case KILL:
+                piece.move(newHorizontal, newVertical);
+                board[oldHorizontal][oldVertical].setPiece(null);
+                board[newHorizontal][newVertical].setPiece(piece);
+                Piece killedPiece = result.getPiece();
+                board[pixelToBoard(killedPiece.getOldHorizontal())][pixelToBoard(killedPiece.getOldVertical())].setPiece(null);
+                pieceGroup.getChildren().remove(killedPiece);
+                turn = (turn+1)%2;
+                break;
+
+        }
+    }
 
 
     private MoveResult tryMove(Piece piece, int newHorizontal, int newVertical)
@@ -277,7 +281,44 @@ class InGame{
         this.board = board;
         this.pieceGroup = pieceGroup;
     }
+    private void makeMove(Piece piece) {
+        System.out.println("-----Called makePiece() on mouse-----");
+        int newHorizontal = pixelToBoard(piece.getLayoutX());
+        int oldHorizontal = pixelToBoard(piece.getOldHorizontal());
+        int newVertical = pixelToBoard(piece.getLayoutY());
+        int oldVertical = pixelToBoard(piece.getOldVertical());
+        MoveResult result = tryMove(piece, newHorizontal, newVertical);
+        if(turn == 0){
+            if (piece.getPieceType()== PieceTypeEnum.TIGER){piece.abortMove(); return;}
+        }
+        else if(turn==1) {
+            if(!(piece.getPieceType()==PieceTypeEnum.TIGER)){piece.abortMove(); return;}
+        }
 
+
+        switch (result.getType()) {
+            case NONE:
+                piece.abortMove();
+                break;
+            case NORMAL:
+                piece.move(newHorizontal, newVertical);
+                System.out.println("Normal move from:: " + oldHorizontal + ", " + oldVertical);
+                board[oldHorizontal][oldVertical].setPiece(null);
+                board[newHorizontal][newVertical].setPiece(piece);
+                turn = (turn+1)%2;
+                break;
+            case KILL:
+                piece.move(newHorizontal, newVertical);
+                board[oldHorizontal][oldVertical].setPiece(null);
+                board[newHorizontal][newVertical].setPiece(piece);
+                Piece killedPiece = result.getPiece();
+                board[pixelToBoard(killedPiece.getOldHorizontal())][pixelToBoard(killedPiece.getOldVertical())].setPiece(null);
+                pieceGroup.getChildren().remove(killedPiece);
+                turn = (turn+1)%2;
+                break;
+
+        }
+    }
     private int pixelToBoard(double pixel) {
         return (int) pixel / (BoardStage.positionSize * 2);
     }
