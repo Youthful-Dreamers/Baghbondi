3313a6601598ce525dbb28ee63b54a3348c34b1e
JaberHPranto <61055952+JaberHPranto@users.noreply.github.com>
2020-09-16 22:07:52 +0600

    music start with game start ...but have more works

---
+++ src/NewBaghbondi/GameAudio.java
@@ -0,0 +1,19 @@
+// have to change....just wrote shameless green
+package NewBaghbondi;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+import java.nio.file.Paths;
+
+public class GameAudio {
+    MediaPlayer mediaPlayer;
+    public void introAudio(){
+        String path="resources/gameIntroMusic.mp3";
+//      Media media=new Media(new File(path).toURI().toString());
+        Media media=new Media(Paths.get(path).toUri().toString());
+        mediaPlayer=new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+}
--- src/NewBaghbondi/Main.java
+++ src/NewBaghbondi/Main.java
@@ -1,7 +1,13 @@
 package NewBaghbondi;
 
 import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 public class Main extends Application{
 
@@ -11,11 +17,20 @@ public class Main extends Application{
         Menu mainMenu = new Menu(primaryStage);
         primaryStage.setScene(mainMenu.createSceneLanguageOption());
         primaryStage.setTitle("BaghBondi");
+        introAudio();
         primaryStage.show();
     }
 
     public static void main(String[] args)
     {
         launch(args);
+    }
+
+
+    public void introAudio(){
+      String path="src/resources/gameIntroMusic.mp3";
+      Media media=new Media(new File(path).toURI().toString());
+       MediaPlayer mediaPlayer=new MediaPlayer(media);
+        mediaPlayer.play();
     }
 }
